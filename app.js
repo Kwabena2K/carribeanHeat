ff7fc7829708da11edb15e869b8b60b52d4aa46e
Kwabena <kwabenao2k@gmail.com>
2022-10-11 19:15:12 -0400

    Setup initial file structure with server connected to localhost. Also installed needed dependencies

---
+++ app.js
@@ -0,0 +1,21 @@
+// Imports
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+require("dotenv").config();
+
+app.use(express.urlencoded({ extended: true }));
+// Creating a static folder for files such as images to have a clear path
+app.use(express.static("public"));
+app.use(expressLayouts);
+
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+// Creating our routes
+const routes = require("./server/routes/recipeRoutes.js");
+app.use("/", routes);
+
+app.listen(port, () => console.log(`Listening to the port ${port}`));
